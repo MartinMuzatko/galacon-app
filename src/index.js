4173b16a89ec474548350d9bcde53b44df87d34c
Martin Muzatko <martin@happy-css.com>
2016-06-12 22:19:28 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import './tags/app.html'
 import './tags/card.html'
 import './tags/cards/event.html'
 import './tags/cards/guest.html'
+import './tags/cards/room.html'
+import './tags/cards/vendor.html'
+import './tags/cards/venue.html'
 import './less/main.less'
 
 riot.mount('*')
