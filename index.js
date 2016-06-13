4b7ef13afb29af384dac8bda146f68619dde7a70
Martin Muzatko <martin@happy-css.com>
2016-06-13 21:22:16 +0200

    Introduced new cards: ludwigsburg map, rules and team

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+import 'riot'
+
 import './tags/app.html'
 import './tags/card.html'
+
+import './tags/cards/vendor.html'
 import './tags/cards/event.html'
 import './tags/cards/guest.html'
 import './tags/cards/room.html'
-import './tags/cards/vendor.html'
+
+import './tags/cards/ludwigsburg.html'
+
 import './tags/cards/venue.html'
+import './tags/cards/team.html'
+import './tags/cards/rules.html'
+
 import './less/main.less'
 
 riot.mount('*')
